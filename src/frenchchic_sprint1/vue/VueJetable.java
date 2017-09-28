e493e21a8658b5121dd5e5fd75340f9a9cda730f
PC_IMED\ASUS <imed.zaghdane.93@gmail.com>
2017-09-28 09:22:32 +0200

    Vue jetable modifié

--- src/frenchchic_sprint1/vue/VueJetable.java
+++ src/frenchchic_sprint1/vue/VueJetable.java
@@ -6,8 +6,18 @@
 package frenchchic_sprint1.vue;
 
 import frenchchic_sprint1.controleur.Session;
+import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
 
 /**
  *
@@ -16,7 +26,13 @@ import javax.swing.JFrame;
 
 // vue jetable
 public class VueJetable extends JFrame {
-    
+    private JTextField tfUsername;
+    private JPasswordField pfPassword;
+    private JLabel lbUsername;
+    private JLabel lbPassword;
+    private JButton btnLogin;
+    private JButton btnCancel;
+    private boolean succeeded;
     
     public VueJetable() {
         this.setTitle("French Chic");
@@ -25,7 +41,89 @@ public class VueJetable extends JFrame {
         this.setLocation(200, 200);
         this.pack();
         this.setVisible(true);
+        
+    
+ 
+    
+        JPanel panel = new JPanel(new GridBagLayout());
+        GridBagConstraints cs = new GridBagConstraints();
+ 
+        cs.fill = GridBagConstraints.HORIZONTAL;
+ 
+        lbUsername = new JLabel("Username: ");
+        cs.gridx = 0;
+        cs.gridy = 0;
+        cs.gridwidth = 1;
+        panel.add(lbUsername, cs);
+ 
+        tfUsername = new JTextField(20);
+        cs.gridx = 1;
+        cs.gridy = 0;
+        cs.gridwidth = 2;
+        panel.add(tfUsername, cs);
+ 
+        lbPassword = new JLabel("Password: ");
+        cs.gridx = 0;
+        cs.gridy = 1;
+        cs.gridwidth = 1;
+        panel.add(lbPassword, cs);
+ 
+        pfPassword = new JPasswordField(20);
+        cs.gridx = 1;
+        cs.gridy = 1;
+        cs.gridwidth = 2;
+        panel.add(pfPassword, cs);
+        
+ 
+        btnLogin = new JButton("Login");
+ 
+        btnLogin.addActionListener(new ActionListener() {
+ 
+            /*public void actionPerformed(ActionEvent e) {
+                if (Login.authenticate(getUsername(), getPassword())) {
+                    JOptionPane.showMessageDialog(LoginDialog.this,
+                            "Hi " + getUsername() + "! You have successfully logged in.",
+                            "Login",
+                            JOptionPane.INFORMATION_MESSAGE);
+                    succeeded = true;
+                    dispose();
+                } else {
+                    JOptionPane.showMessageDialog(LoginDialog.this,
+                            "Invalid username or password",
+                            "Login",
+                            JOptionPane.ERROR_MESSAGE);
+                    // reset username and password
+                    tfUsername.setText("");
+                    pfPassword.setText("");
+                    succeeded = false;
+ 
+                }
+            }*/
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+        });
+        btnCancel = new JButton("Cancel");
+        btnCancel.addActionListener(new ActionListener() {
+ 
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+            }
+        });
+        JPanel bp = new JPanel();
+        bp.add(btnLogin);
+        bp.add(btnCancel);
+ 
+        getContentPane().add(panel, BorderLayout.CENTER);
+        getContentPane().add(bp, BorderLayout.PAGE_END);
+ 
+        pack();
+        setResizable(false);
+        //setLocationRelativeTo(parent);
     }
+    
 
     public static void main(String[] args) {
         //EcranAccueil ecranAccueil = 
